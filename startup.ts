b235926687f60551205c13a8e5ce00f142be8e0c
jayvalentine <jayv136@gmail.com>
2019-05-24 17:52:03 +0100

    Got fadein working

---
+++ startup.ts
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  $("#StartTitle").hide();
+  $("#StartText").hide();
+
+
+  $("#StartTitle").fadeIn(4000, function () {
+    $("#StartText").fadeIn(1000, function () {});
+  });
+
+})
