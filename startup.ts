a2aad0dc05b8d31baaf6c16a588dd86d4a5c5377
jayvalentine <jayv136@gmail.com>
2019-05-29 15:39:34 +0100

    Added version string and events count to start screen.
    Added the version string and the number of events in the game to the start
    screen.

--- startup.ts
+++ startup.ts
@@ -1,11 +1,15 @@
+/// <reference path="./tribe_events.ts">
 
 $(document).ready(function() {
+  $("#VersionString").text(`Version 0.1 (${TribeEvents.length} events)`);
   $("#StartTitle").hide();
   $("#StartText").hide();
-
+  $("#StartVersionInfo").hide();
 
   $("#StartTitle").fadeIn(4000, function () {
-    $("#StartText").fadeIn(1000, function () {});
+    $("#StartText").fadeIn(1000, function () {
+      $("#StartVersionInfo").fadeIn(500, function () {})
+    });
   });
 
 })
